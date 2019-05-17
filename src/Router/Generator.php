86a192360a80a5ed5544a99361de21b9d5331a39
wangmin <stlswm@163.com>
2019-05-17 15:35:42 +0800

    up

---
+++ src/Router/Generator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace stlswm\LaravelSupplement\Router;
+
+/**
+ * Class Generator
+ * 路由生成器
+ *
+ * @package stlswm\LaravelSupplement\Router
+ */
+class Generator
+{
+    /**
+     * 开始生成
+     *
+     * @param string $dir
+     */
+    public static function start(string $dir)
+    {
+
+    }
+}

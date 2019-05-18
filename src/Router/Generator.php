125f235335d55af7199afc1f6ea2f58a10535509
wangmin <stlswm@163.com>
2019-05-18 10:19:57 +0800

    up

--- src/Router/Generator.php
+++ src/Router/Generator.php
@@ -2,6 +2,8 @@
 
 namespace stlswm\LaravelSupplement\Router;
 
+use Exception;
+
 /**
  * Class Generator
  * 路由生成器
@@ -10,13 +12,115 @@ namespace stlswm\LaravelSupplement\Router;
  */
 class Generator
 {
+    public static $cache = [
+        'api' => [
+            'use Illuminate\Support\Facades\Route;',
+            '',
+            ''
+        ],
+        'web' => [
+            'use Illuminate\Support\Facades\Route;',
+            '',
+            ''
+        ],
+    ];
+
+    /**
+     * @param string $msg
+     */
+    protected static function println(string $msg)
+    {
+        echo $msg, "\n";
+    }
+
     /**
      * 开始生成
      *
-     * @param string $dir
+     * @param string $readDir
+     * @param string $routerDir
+     *
+     * @router get /var/www
+     *
+     * @throws Exception
      */
-    public static function start(string $dir)
+    public static function start(string $readDir, string $routerDir)
     {
+        if (!is_dir($readDir)) {
+            throw new Exception($readDir . 'is not a dir');
+        }
+        $dh = opendir($readDir);
+        while ($file = readdir($dh)) {
+            if ($file != '.' && $file != '..') {
+                $newPath = $readDir . '/' . $file;
+                if (is_dir($newPath)) {
+                    self::start($newPath, $routerDir);
+                } else {
+                    self::parseFile($newPath);
+                }
+            }
+        }
+        closedir($dh);
+        file_put_contents($routerDir . '/api.php', join("\n", self::$cache['api']));
+        file_put_contents($routerDir . '/web.php', join("\n", self::$cache['web']));
+        self::println('done');
+    }
 
+    /**
+     * @param string $file
+     * @param string $routerDir
+     */
+    /**
+     * @param string $file
+     *
+     * @throws Exception
+     */
+    public static function parseFile(string $file)
+    {
+        $namespace = '';
+        $controller = '';
+        $router = '';
+        $fh = fopen($file, 'r');
+        self::$cache['api'][] = '';
+        self::$cache['web'][] = '';
+        while ($line = fgets($fh)) {
+            if (!$namespace) {
+                preg_match('/^(?:\s+)?namespace\s(.*?);/', $line, $matched);
+                if ($matched) {
+                    $namespace = str_replace("\\", "\\\\", $matched[1]);
+                    $namespace = str_replace('App\\\\Http\\\\Controllers\\\\', '', $namespace);
+                    continue;
+                }
+            }
+            if (!$controller) {
+                preg_match('/^(?:\s+)?class (\w+){?/', $line, $matched);
+                if ($matched) {
+                    $controller = $matched[1];
+                    continue;
+                }
+            }
+            preg_match('/^(?:\s+)?\*(?:\s+)?@router\s(.*?)\n$/', $line, $matched);
+            if ($matched) {
+                $routerSearch = explode(' ', $matched[1]);
+                if (!$routerSearch || count($routerSearch) < 2) {
+                    throw new Exception('error format:' . $line);
+                }
+                $router = $routerSearch[0] . '("' . $routerSearch[1] . '",';
+                continue;
+            }
+            preg_match('/^(?:\s+)?public function (\w+)(?:\()?/', $line, $matched);
+            if ($matched) {
+                $function = $matched[1];
+                if ($router && $function) {
+                    if (strpos($router, '"/api') !== FALSE) {
+                        self::$cache['api'][] = 'Route::' . $router . '"' . $namespace . "\\\\" . $controller . '@' . $function . '");';
+                    } else {
+                        self::$cache['web'][] = 'Route::' . $router . '"' . $namespace . "\\\\" . $controller . '@' . $function . '");';
+                    }
+                }
+                $router = '';
+                continue;
+            }
+        }
+        fclose($fh);
     }
 }

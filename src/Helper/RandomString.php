9565051433d584325f247ae67f2a61dd4ba297f6
George <jobsteng@163.com>
2019-07-16 02:37:13 +0000

    生成随机字符串

---
+++ src/Helper/RandomString.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace stlswm\LaravelSupplement\Helper;
+
+/**
+ * Class RandomString
+ * @package  stlswm\LaravelSupplement\Helper
+ * @author   George
+ * @datetime 2019/7/16 10:25
+ * @annotation
+ */
+class RandomString
+{
+    /**
+     * @var string
+     * @annotation 随机字符串种子
+     */
+    public static $seek = '01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+
+    /**
+     * @param int $length
+     * @return string
+     * @annotation 生成传入长度的随机字符串
+     */
+    public static function getStr(int $length = 32): string
+    {
+        return substr(str_shuffle(self::$seek), 0, $length);
+    }
+}
